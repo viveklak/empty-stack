04ad98c163f68e059ae793728d0080079e77ad8f
Vivek Lakshmanan <vivek@pulumi.com>
2021-04-21 00:46:38 -0700

    Load secret

--- index.ts
+++ index.ts
@@ -1,2 +1,9 @@
+import * as pulumi from "@pulumi/pulumi";
+
+const cfg = new pulumi.Config();
+const password = cfg.getSecret("dbPassword");
 console.log("Empty stack!");
 
+if (password != undefined) {
+    password.apply(p => console.log(`Password is ${p}`));
+}
