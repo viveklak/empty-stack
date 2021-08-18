627e7fb875cf16e2a84d1c9fb82d132eedf4fffe
Vivek Lakshmanan <viveklak@gmail.com>
2021-08-17 22:35:33 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,5 +4,5 @@ const cfg = new pulumi.Config();
 const password = cfg.requireSecret("dbPassword");
 console.log("Empty stack!");
 
-export const notSoSecret = pulumi.secret("safe");
+export const notSoSecret = pulumi.output("safe");
 export const secretVal = pulumi.secret("newSecret");
