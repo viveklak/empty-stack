abb2a81755c4ebb3c666a4979751bcf878489a97
Vivek Lakshmanan <viveklak@gmail.com>
2021-08-20 16:22:12 +0000

    Add config

--- index.ts
+++ index.ts
@@ -2,5 +2,7 @@ import * as pulumi from "@pulumi/pulumi";
 
 console.log("Empty stack!");
 
+const cfg = new pulumi.Config("aws");
+export const region = cfg.require("region");
 export const notSoSecret = pulumi.output("safe");
 export const secretVal = pulumi.secret("newSecret");
