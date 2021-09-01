09b7c0916a25dd3f15cf6ec8c40e35e823630e47
Vivek Lakshmanan <vivek@lakshmanan.ca>
2021-09-01 12:59:27 -0700

    Add a wait

--- index.ts
+++ index.ts
@@ -6,3 +6,5 @@ const cfg = new pulumi.Config("aws");
 export const region = cfg.require("region");
 export const notSoSecret = pulumi.output("safe");
 export const secretVal = pulumi.secret("newSecret");
+
+await new Promise(resolve => setTimeout(resolve, 60000));
